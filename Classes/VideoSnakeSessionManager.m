0cf716c37a1f64b31a3b051d98a261d350237b46
OOPer <nagata@shlab.jp>
2017-02-12 16:46:01 +0900

    Updated base sample code to Version 4.1

--- Classes/VideoSnakeSessionManager.m
+++ Classes/VideoSnakeSessionManager.m
@@ -1,49 +1,6 @@
 
 /*
-     File: VideoSnakeSessionManager.m
- Abstract: The class that creates and manages the AVCaptureSession
-  Version: 2.2
- 
- Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
- Inc. ("Apple") in consideration of your agreement to the following
- terms, and your use, installation, modification or redistribution of
- this Apple software constitutes acceptance of these terms.  If you do
- not agree with these terms, please do not use, install, modify or
- redistribute this Apple software.
- 
- In consideration of your agreement to abide by the following terms, and
- subject to these terms, Apple grants you a personal, non-exclusive
- license, under Apple's copyrights in this original Apple software (the
- "Apple Software"), to use, reproduce, modify and redistribute the Apple
- Software, with or without modifications, in source and/or binary forms;
- provided that if you redistribute the Apple Software in its entirety and
- without modifications, you must retain this notice and the following
- text and disclaimers in all such redistributions of the Apple Software.
- Neither the name, trademarks, service marks or logos of Apple Inc. may
- be used to endorse or promote products derived from the Apple Software
- without specific prior written permission from Apple.  Except as
- expressly stated in this notice, no other rights or licenses, express or
- implied, are granted by Apple herein, including but not limited to any
- patent rights that may be infringed by your derivative works or by other
- works in which the Apple Software may be incorporated.
- 
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
- MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
- THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
- OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- 
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
- MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
- AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
- STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
- 
- Copyright (C) 2014 Apple Inc. All Rights Reserved.
- 
+ <codex/>
  */
 
 #import "VideoSnakeSessionManager.h"
@@ -111,7 +68,7 @@ static CGFloat angleOffsetFromPortraitOrientationToOrientation(AVCaptureVideoOri
 
 @interface VideoSnakeSessionManager () <AVCaptureAudioDataOutputSampleBufferDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, MovieRecorderDelegate, MotionSynchronizationDelegate>
 {
-	__weak id <VideoSnakeSessionManagerDelegate> _delegate; // __weak doesn't actually do anything under non-ARC
+	id <VideoSnakeSessionManagerDelegate> _delegate;
 	dispatch_queue_t _delegateCallbackQueue;
 	
 	NSMutableArray *_previousSecondTimestamps;
@@ -837,7 +794,7 @@ static CGFloat angleOffsetFromPortraitOrientationToOrientation(AVCaptureVideoOri
 			transform = CGAffineTransformScale(transform, -1, 1);
 		}
 		else {
-			if ( UIInterfaceOrientationIsPortrait(orientation) ) {
+			if ( UIInterfaceOrientationIsPortrait((UIInterfaceOrientation)orientation) ) {
 				transform = CGAffineTransformRotate(transform, M_PI);
 			}
 		}
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Translated by OOPer in cooperation with shlab.jp, on 2015/3/28.
 
 Based on
-<https://developer.apple.com/library/ios/samplecode/VideoSnake/Introduction/Intro.html#//apple_ref/doc/uid/DTS40012327>
-2014-09-18.
+<https://developer.apple.com/library/content/samplecode/VideoSnake/Introduction/Intro.html#//apple_ref/doc/uid/DTS40012327>
+2016-09-28.
 
 As this is a line-by-line translation from the original sample code, "redistribute the Apple Software in its entirety and without modifications" would apply. See license terms in each file.
 Some faults caused by my translation may exist. Not all features tested.
